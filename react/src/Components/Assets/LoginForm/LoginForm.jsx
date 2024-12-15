06e3650e3519a4771f8e5d61fa14d19d676661e3
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2024-12-15 20:55:09 +0530

    Added signup form

--- react/src/Components/Assets/LoginForm/LoginForm.jsx
+++ react/src/Components/Assets/LoginForm/LoginForm.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import './LoginForm.css'
+import React from 'react';
+import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
-
-
-
+import { Link } from 'react-router-dom';
 
 export const LoginForm = () => {
   return (
@@ -11,23 +9,23 @@ export const LoginForm = () => {
         <form action="">
             <h1>Login</h1>
             <div className='input-box'>
-                <input type='text' placeholder='Username' required/>
-                <FaUser className='icon'/>
+                <input type='text' placeholder='Username' required />
+                <FaUser className='icon' />
             </div>
             <div className='input-box'>
-                <input type = 'password' placeholder = 'Password' required/>
-                <FaLock className='icon'/>
+                <input type='password' placeholder='Password' required />
+                <FaLock className='icon' />
             </div>
             <div className="remember-forget">
-                <label><input type='checkbox' /> Remember me  </label>
+                <label><input type='checkbox' /> Remember me</label>
                 <a href="#" className='forgotpass'>Forgot password?</a>
             </div>
 
             <button type='submit'>Login</button>
             <div className="register-link">
-                <p> Don't have an account? <a href="#">Register</a></p>
+                <p>Don&apos;t have an account? <Link to="/signup">Register</Link></p>
             </div>
         </form>
     </div>
-  )
-}
+  );
+};
